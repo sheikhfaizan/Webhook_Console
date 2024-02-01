25b46397a9242ca804576be377d6392f0f6a4f4a
sheikhfaizan <sheikhfaizanameen@gmail.com>
2024-02-01 22:07:40 +0500

    added 3 calls, get customer from cin7, create, update item in monday

---
+++ Webhook_Console/Models/ExceptionLog.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webhook_Console.Models
+{
+    public static class Log
+    {
+
+        public static void DataLog(string data, bool isFirst = false)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory; // You get main rott
+                string filePath = Path.Combine(path, "Logs", $"{DateTime.Now:yyyy-MM-dd}.txt"); //"LogsFile.txt"
+
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath).Dispose();
+                }
+
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    if (isFirst)
+                        writer.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                    else
+                        writer.WriteLine("-----------------------------------------------------------------------------");
+                    writer.WriteLine("Date : " + DateTime.Now.ToString());
+                    writer.WriteLine();
+                    writer.WriteLine(data);
+
+                }
+            }
+            catch (Exception exx)
+            {
+                throw new Exception("File log exception: " + exx.Message, exx.InnerException); ;
+            }
+        }
+        public static void ExceptionLog(Exception ex)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory; // You get main rott
+                string filePath = Path.Combine(path, "Logs", $"{DateTime.Now:yyyy-MM-dd}.txt"); // $"{DateTime.Now:yyyy-MM-dd}.txt");
+
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath).Dispose();
+                }
+
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine("-----------------------------------------------------------------------------");
+                    writer.WriteLine("Date : " + DateTime.Now.ToString());
+                    writer.WriteLine();
+
+                    while (ex != null)
+                    {
+                        writer.WriteLine(ex.GetType().FullName);
+                        writer.WriteLine("Message : " + ex.Message);
+                        writer.WriteLine("Inner Exception : " + ex.InnerException);
+                        writer.WriteLine("StackTrace : " + ex.StackTrace);
+
+                        ex = ex.InnerException;
+                    }
+                }
+            }
+            catch (Exception exx)
+            {
+                throw new Exception("File log exception: " + exx.Message, exx.InnerException); ;
+            }
+
+        }
+    }
+}
---
+++ Webhook_Console/Models/ItemsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webhook_Console.Models
+{
+    internal class ItemsModel
+    {
+        public class Board
+        {
+            public ItemsPage Items_Page { get; set; }
+        }
+
+        public List<Board> Boards { get; set; }
+
+        public class Item
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class ItemsPage
+        {
+            public object Cursor { get; set; }
+            public List<Item> Items { get; set; }
+        }
+
+    }
+}
